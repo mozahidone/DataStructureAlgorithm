b29e6c65eae67b06d72b1cbc75256990875a6ade
Mozahid <mozahidone@gmail.com>
2022-07-27 22:03:29 +0600

    Code test: Find out smallest and largest number in a given array

---
+++ data-structure/src/main/java/com/mozahidone/array/CodeTest.java
@@ -0,0 +1,20 @@
+package com.mozahidone.array;
+
+// Find out smallest and largest number in a given array.
+
+public class CodeTest {
+
+    public static void main(String[] args) {
+        int[] nummbers = {10, 3, 22, 5, 99, 7};
+        int smallest = nummbers[0], largest = nummbers[0];
+
+        for (int number: nummbers) {
+            if (number > largest)
+                largest = number;
+            else if (number < smallest)
+                smallest = number;
+        }
+
+        System.out.println("Smallest: " + smallest + ", Largest: " + largest);
+    }
+}
