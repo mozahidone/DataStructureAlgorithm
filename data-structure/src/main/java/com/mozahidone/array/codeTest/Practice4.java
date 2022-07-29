50fef36b53e8aa8fc17e69dd6c51d36717415867
Mozahid <mozahidone@gmail.com>
2022-07-29 16:29:58 +0600

    Finding the Largest Element in an Array

---
+++ data-structure/src/main/java/com/mozahidone/array/codeTest/Practice4.java
@@ -0,0 +1,19 @@
+package com.mozahidone.array.codeTest;
+
+// Finding the Largest Element in an Array
+public class Practice4 {
+    public static void main(String[] args) {
+        int[] numbers = {222,11,999,22,4,66,99};
+        System.out.println(findLargestElement(numbers));
+    }
+
+    public static int findLargestElement(int[] numbers) {
+        int largestElement = -1;
+        for(int i=0; i<numbers.length; i++) {
+            int currentNumber = numbers[i];
+            if(currentNumber > largestElement)
+                largestElement = currentNumber;
+        }
+        return largestElement;
+    }
+}
