3619d3c2fa17e1a37b8462d92e98f78cb69e5095
Mozahid <mozahidone@gmail.com>
2022-07-31 20:07:28 +0600

    Reverse an array

---
+++ data-structure/src/main/java/com/mozahidone/array/codeTest/Practice5.java
@@ -0,0 +1,23 @@
+package com.mozahidone.array.codeTest;
+
+import java.util.Arrays;
+
+// Reverse an array
+public class Practice5 {
+    public static void main(String[] args) {
+        int[] numbers = {5,3,6,4,8,9};
+        System.out.println(Arrays.toString(reverseArray(numbers)));
+    }
+
+    public static int[] reverseArray(int[] numbers) {
+        int i=0, j=numbers.length-1, temp;
+        while(i < j) {
+            temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+            i++;
+            j--;
+        }
+        return numbers;
+    }
+}
