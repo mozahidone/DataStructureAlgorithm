d0d7ec3890617caedee04e73225f30f83f5de61f
mozahid <mozahidone@gmail.com>
2022-08-13 11:45:10 +0600

    Problem solving pattern, double pointer to reverse the array

---
+++ problem-solving/src/main/java/com/mozahidone/arrayProblem/DoublePointer.java
@@ -0,0 +1,23 @@
+package com.mozahidone.arrayProblem;
+
+import java.util.Arrays;
+
+// Reverse an array
+public class DoublePointer {
+    public static void main(String[] args) {
+        int[] numbers = {5,3,6,4,8,9};
+        System.out.println(Arrays.toString(reverseArray(numbers)));
+    }
+
+    public static int[] reverseArray(int[] numbers) {
+        int i=0, j=numbers.length-1, temp;
+        while(i < j) {
+            temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+            i++;
+            j--;
+        }
+        return numbers;
+    }
+}
