05681ace8ab4b54a9fabf1ccc3f10cb1cc23a7ad
mozahid <mozahidone@gmail.com>
2022-08-13 20:37:56 +0600

    Method design: mirror tree

---
+++ data-structure/src/main/java/com/mozahidone/tree/problem/MirrorTree.java
@@ -0,0 +1,17 @@
+package com.mozahidone.tree.problem;
+
+
+import com.mozahidone.tree.Tree;
+
+public class MirrorTree {
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(7);
+        root.right = new Node(15);
+    }
+
+    public static void mirror(Node root) {
+        // todo need to implement
+    }
+}
