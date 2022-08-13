29f58acaf897f75fb3292a96d6d95aab570222b2
mozahid <mozahidone@gmail.com>
2022-08-13 20:00:33 +0600

    Binary tree height implementation

---
+++ data-structure/src/main/java/com/mozahidone/tree/problem/BinaryTreeHeight.java
@@ -0,0 +1,33 @@
+package com.mozahidone.tree.problem;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class BinaryTreeHeight {
+    public static int height(Node root) {
+        if(root == null)
+            return -1;
+        return Math.max(height(root.left), height(root.right))+1;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(7);
+        root.right = new Node(15);
+
+        root.left.left = new Node(3);
+        root.left.right = new Node(8);
+
+        root.right.left = new Node(13);
+        root.right.right = new Node(18);
+
+        System.out.println(height(root));
+    }
+
+}
