c7a6c85ab190e987ff256ecff89a976f3d60b422
mozahid <mozahidone@gmail.com>
2023-03-22 22:35:50 +0600

    Linked list traversal

--- data-structure/src/main/java/com/mozahidone/linkedlist/practice/Traversal.java
+++ data-structure/src/main/java/com/mozahidone/linkedlist/practice/Traversal.java
@@ -27,19 +27,46 @@ public class Traversal {
     }
 
     public static void main(String[] args) {
-        Node a = new Node(1);
-        Node b = new Node(2);
-        Node c = new Node(3);
-        Node d = new Node(4);
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next =new Node(4);
+        node.next.next.next.next =new Node(5);
 
-        a.next = b;
-        b.next = c;
-        c.next =d;
-
-        recursivePrint(a);
+        //recursivePrint(node);
+        iterativePrint(node);
+        node = reverse(node);
+        iterativePrint(node);
         //System.out.println(sum(a));
         //System.out.println(getValueAtIndex(null, 5));
-        System.out.println(getValueAtIndexUsingRecursion(a, 7));
+        //System.out.println(getValueAtIndexUsingRecursion(node, 7));
+    }
+
+    public static Node reverse(Node head) {
+        Node previous = null, current = head, next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+        return head;
+    }
+    public static void recursion(Node head) {
+        if (head == null)
+            return;
+        System.out.println(head.val);
+        recursion(head.next);
+    }
+
+    public static void iterativePrint(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.val);
+            current = current.next;
+        }
     }
 
     static int sum(Node head) {
