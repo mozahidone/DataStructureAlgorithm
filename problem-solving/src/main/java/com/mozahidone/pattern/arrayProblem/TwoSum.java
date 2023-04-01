a3c09729ccb2fe68489eb5dc008ecd3b8e2d6148
mozahid <mozahidone@gmail.com>
2023-04-01 23:41:40 +0600

    Find the indices of any array that matches with the target using hash table approach

--- problem-solving/src/main/java/com/mozahidone/pattern/arrayProblem/TwoSum.java
+++ problem-solving/src/main/java/com/mozahidone/pattern/arrayProblem/TwoSum.java
@@ -1,11 +1,15 @@
 package com.mozahidone.pattern.arrayProblem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public static void main(String[] args) {
         int[] numbers = {2, 7, 6, 11, 13};
         int target = 24;
         System.out.println(bruteForce(numbers, target));
+        System.out.println(hashTable(numbers, target));
     }
 
     public static int[] bruteForce(int[] numbers, int target) {
@@ -21,4 +25,21 @@ public class TwoSum {
         }
         return result;
     }
+
+    public static int[] hashTable(int[] numbers, int target) {
+        int[] result = {-1, -1};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int complement = target - numbers[i];
+            if (map.containsKey(complement)) {
+                result[0] = map.get(complement);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(numbers[i], i);
+            }
+        }
+        return result;
+    }
+
 }
